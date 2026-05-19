db33e24672aa3a1c931c458c381dfeb29872dca5
Joel Hawksley <joelhawksley@github.com>
2026-05-19 09:11:42 -0600

    add e1001 buzzer on wifi reset

--- src/bl.cpp
+++ src/bl.cpp
@@ -42,6 +42,7 @@
 #include "loading.h"
 #include <wifi-helpers.h>
 #include <sys/time.h>
+#include <buzzer.h>
 #ifdef SENSOR_SDA
 #include <bb_scd41.h>
 #include <bb_temperature.h>
@@ -637,6 +638,7 @@ void bl_init(void)
 #endif
   Log_info("BL init success");
   pins_init();
+  buzzer_init();
   vBatt = readBatteryVoltage(); // Read the battery voltage BEFORE WiFi is turned on
 #ifdef SENSOR_SDA
   // check if there is a SCD41 or supported temperature sensor attached
@@ -731,6 +733,7 @@ void bl_init(void)
     {
     case LongPress:
       Log_info("WiFi reset");
+      buzzer_beep_pattern(2, 100, 100);
       WifiCaptivePortal.resetSettings();
       break;
     case DoubleClick:
