4d7ccab760c4adb7cc5166a006dad02176c85874
Joel Hawksley <joelhawksley@github.com>
2026-05-19 09:11:42 -0600

    add e1001 buzzer on wifi reset

--- src/bl.cpp
+++ src/bl.cpp
@@ -44,6 +44,7 @@
 #include "loading.h"
 #include <wifi-helpers.h>
 #include <sys/time.h>
+#include <buzzer.h>
 #ifdef SENSOR_SDA
 #include <bb_scd41.h>
 #include <bb_temperature.h>
@@ -805,6 +806,7 @@ void bl_init(void)
 #endif
   Log_info("BL init success");
   pins_init();
+  buzzer_init();
   sensor_init();
 #ifdef BOARD_TRMNL_X
   // Debug: Print all wakeup_stub_iqs_status structure fields
@@ -862,6 +864,7 @@ void bl_init(void)
     {
     case LongPress:
       Log_info("WiFi reset");
+      buzzer_beep_pattern(2, 100, 100);
       WifiCaptivePortal.resetSettings();
       break;
     case DoubleClick:
